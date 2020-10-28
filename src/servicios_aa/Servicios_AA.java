1202643c18727fb9b05922518570166430b4f893
Leude <61247560+Leude@users.noreply.github.com>
2020-10-28 11:41:29 -0600

    Update Servicios_AA.java

--- src/servicios_aa/Servicios_AA.java
+++ src/servicios_aa/Servicios_AA.java
@@ -2,7 +2,7 @@ package servicios_aa;
 
 public class Servicios_AA {
    public static void main(String args[]){
-       Pantalla vista = new Pantalla();
+       Pantalla vista = new Pantalla(); //Esta es la pantalla
        Controlador control = new Controlador(vista);
    }
 }
