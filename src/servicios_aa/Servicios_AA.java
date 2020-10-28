9abe40af32f0fafae95c718c93d9f09d4865a7de
RICARDO <rikiton.raam@gmail.com>
2020-10-28 11:19:14 -0600

    Empezando el proyecto 2

--- src/servicios_aa/Servicios_AA.java
+++ src/servicios_aa/Servicios_AA.java
@@ -1,10 +1,8 @@
 package servicios_aa;
 
-import interfaces.Control_de_Asistencias;
-
 public class Servicios_AA {
    public static void main(String args[]){
-       Control_de_Asistencias ca = new Control_de_Asistencias();
-       ca.setVisible(true);
+       Pantalla vista = new Pantalla();
+       Controlador control = new Controlador(vista);
    }
 }
