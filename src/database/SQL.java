4eecca9f3e2f560070949bd65df28e5147da80b9
RICARDO <rikiton.raam@gmail.com>
2020-10-29 10:35:51 -0600

    Una mini actualizacion en comentarios

--- src/database/SQL.java
+++ src/database/SQL.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import oracle.jdbc.pool.OracleDataSource;
 
 public class SQL {
-//Todavia en proceso de programar 1234
+//Todavia en proceso de programar 12344
     public void abrir() throws SQLException {
         OracleDataSource ods = new OracleDataSource();
         ods.setURL("jdbc:oracle:thin:@//localhost:1521/XEPDB1"); 
