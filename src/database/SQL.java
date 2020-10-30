61174f36a4cbffe70e6fa7546c313f80ea0bf2f6
RICARDO <rikiton.raam@gmail.com>
2020-10-30 09:38:34 -0600

    Una mini actualizacion en comentarios

--- src/database/SQL.java
+++ src/database/SQL.java
@@ -1,19 +1,17 @@
 //Futura interfaz del proyecto
 package database;
-//puede cambiarse a import java.sql.*
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
+import java.sql.*;
 import oracle.jdbc.pool.OracleDataSource;
 
 public class SQL {
 //Todavia en proceso de programar 12344
+
     public void abrir() throws SQLException {
         OracleDataSource ods = new OracleDataSource();
-        ods.setURL("jdbc:oracle:thin:@//localhost:1521/XEPDB1"); 
-        ods.setUser("sistema"); 
-        ods.setPassword("GetStarted18c"); 
+        ods.setURL("jdbc:oracle:thin:@//localhost:1521/XEPDB1");
+        ods.setUser("sistema");
+        ods.setPassword("GetStarted18c");
         Connection conn = ods.getConnection();
         PreparedStatement stmt = conn.prepareStatement("SELECT 'Hello World!' FROM dual");
         ResultSet rslt = stmt.executeQuery();
@@ -22,4 +20,3 @@ public class SQL {
         }
     }
 }
-
